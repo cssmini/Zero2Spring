ba8e00331083769b0a4aa259c9796312204e0e93
cssmini <cssmini@foxmail.com>
2026-04-04 02:10:12 +0900

    AppConfig add test function add

---
+++ src/main/java/com/cssmini/AppConfig.java
@@ -0,0 +1,13 @@
+package com.cssmini;
+
+
+import com.spring.annotation.ComponentScan;
+
+/**
+ * @author Ka KinRai
+ * @date 2026/4/4 02:04
+ * @description
+ */
+@ComponentScan("com.cssmini.service.Impl")
+public class AppConfig {
+}
--- src/main/java/com/cssmini/test.java
+++ src/main/java/com/cssmini/test.java
@@ -1,10 +1,21 @@
 package com.cssmini;
 
 
+import com.cssmini.service.UserService;
+import com.spring.ApplicationContext;
+
 /**
  * @author Ka KinRai
  * @date 2026/4/4 00:55
  * @description
  */
 public class test {
+    public static void main(String[] args) {
+        // spring init
+        ApplicationContext applicationContext = new ApplicationContext(AppConfig.class);
+        // get spring bean
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        // userService function running
+        userService.test();
+    }
 }
