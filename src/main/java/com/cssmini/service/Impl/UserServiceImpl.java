937e2f8dc9a3d52f0b7ab6103a938f8ec47c8328
cssmini <cssmini@foxmail.com>
2026-04-04 01:49:37 +0900

    test class of spring feature add

---
+++ src/main/java/com/cssmini/service/Impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.cssmini.service.Impl;
+
+
+import com.cssmini.service.UserService;
+import com.spring.annotation.Component;
+
+/**
+ * @author Ka KinRai
+ * @date 2026/4/4 01:40
+ * @description
+ */
+@Component
+public class UserServiceImpl implements UserService {
+    @Override
+    public void test() {
+
+    }
+}
---
+++ src/main/java/com/cssmini/service/UserService.java
@@ -0,0 +1,11 @@
+package com.cssmini.service;
+
+
+/**
+ * @author Ka KinRai
+ * @date 2026/4/4 01:39
+ * @description
+ */
+public interface UserService {
+    public void test();
+}
